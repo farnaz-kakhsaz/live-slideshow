b7b9802c8887c5843e072a6acf3b1945f51b1021
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2019-11-01 00:01:45 +0330

    add build

---
+++ build/precache-manifest.60b319ad28ded691be0e8e836216a3d0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7449c1178a6382331fda8913944ff4",
+    "url": "/Slideshow/index.html"
+  },
+  {
+    "revision": "1a7cdbaeba0b3c1b445f",
+    "url": "/Slideshow/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d439ae661019a932920a",
+    "url": "/Slideshow/static/js/2.5c3f5dd9.chunk.js"
+  },
+  {
+    "revision": "1a7cdbaeba0b3c1b445f",
+    "url": "/Slideshow/static/js/main.0b7174cd.chunk.js"
+  },
+  {
+    "revision": "5e01027b1e74c08a71bf",
+    "url": "/Slideshow/static/js/runtime-main.b3175772.js"
+  },
+  {
+    "revision": "4de03ef528f97a6eed383403a53488f9",
+    "url": "/Slideshow/static/media/image-1.4de03ef5.jpg"
+  },
+  {
+    "revision": "7a3427aa8f6859c767cd33c98a76355c",
+    "url": "/Slideshow/static/media/image-10.7a3427aa.jpg"
+  },
+  {
+    "revision": "d4fbd49b569f9cee045e956f7c11b846",
+    "url": "/Slideshow/static/media/image-11.d4fbd49b.jpg"
+  },
+  {
+    "revision": "0b8ebac2d0fe10b9f43b80bbfa4c3cb6",
+    "url": "/Slideshow/static/media/image-12.0b8ebac2.jpg"
+  },
+  {
+    "revision": "8519be52137da95d676565e26eb49a04",
+    "url": "/Slideshow/static/media/image-2.8519be52.jpg"
+  },
+  {
+    "revision": "4aeb2a8a510d68c0a045d94e3ace3498",
+    "url": "/Slideshow/static/media/image-3.4aeb2a8a.jpg"
+  },
+  {
+    "revision": "105758db6145db1c8d7fa6a74c6316db",
+    "url": "/Slideshow/static/media/image-4.105758db.jpg"
+  },
+  {
+    "revision": "2509fe25018b0e1a117f279bdb78c6ec",
+    "url": "/Slideshow/static/media/image-5.2509fe25.jpg"
+  },
+  {
+    "revision": "f3f1688607168022273f9d473e73be5e",
+    "url": "/Slideshow/static/media/image-6.f3f16886.jpg"
+  },
+  {
+    "revision": "73d60203af923a72b8862e950a6f96eb",
+    "url": "/Slideshow/static/media/image-7.73d60203.jpg"
+  },
+  {
+    "revision": "772b89badb5b4e72619de7392053a9fc",
+    "url": "/Slideshow/static/media/image-8.772b89ba.jpg"
+  },
+  {
+    "revision": "7c9e9798aa4201a004a6e9db487fd6b5",
+    "url": "/Slideshow/static/media/image-9.7c9e9798.jpg"
+  }
+]);
