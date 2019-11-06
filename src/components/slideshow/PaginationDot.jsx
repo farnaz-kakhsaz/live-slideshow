d642c375d1afe878670a4fa5129f97b32f564dc4
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2019-11-07 01:42:26 +0330

    add marginRight to all dots

--- src/components/slideshow/PaginationDot.jsx
+++ src/components/slideshow/PaginationDot.jsx
@@ -6,13 +6,14 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const useStyles = makeStyles(theme => ({
   dot: {
-    "&:hover":{
-      background:theme.palette.grey[700]
+    "&:hover": {
+      background: theme.palette.grey[700]
     },
     backgroundColor: theme.palette.grey[400],
     width: theme.spacing(2),
     height: theme.spacing(2),
-    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(0.5),
+    marginLeft: theme.spacing(0.5),
     borderRadius: "50%",
     border: "none",
     cursor: "pointer",
