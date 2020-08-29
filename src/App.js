7dbab7397451c45dd1f4ee982d0458286d57f421
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2020-08-29 21:47:24 +0430

    add theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import HomePage from "./components"
+import React from "react";
+// Components
+import { theme } from "./theme";
+import HomePage from "./components";
 // Material-UI
-import CssBaseline from "@material-ui/core/CssBaseline"
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
-
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <HomePage />
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
 export default App;
