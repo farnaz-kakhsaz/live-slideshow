6ddb6586e3f2e4dee58d64b7f5493f11bfa33955
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2020-08-29 21:46:52 +0430

    change maxWidth of Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,31 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from "react";
+import PropTypes from "prop-types";
 // Material-UI
-import makeStyles from "@material-ui/core/styles/makeStyles"
-import Box from "@material-ui/core/Box"
-import Typography from "@material-ui/core/Typography"
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   image: {
-    maxWidth: 400,
-    width:"100%",
+    maxWidth: 375,
+    width: "100%",
     borderRadius: 10,
-    marginBottom: theme.spacing(1)
-  }
-}))
+    marginBottom: theme.spacing(1),
+  },
+}));
 
 export default function Card({ image, text }) {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
     <Box textAlign="center" color="grey.400">
       <img className={classes.image} src={image} alt={text} />
-      <Typography>
-        {text}
-      </Typography>
+      <Typography>{text}</Typography>
     </Box>
-  )
+  );
 }
 
 Card.propTypes = {
   image: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
-}
+};
