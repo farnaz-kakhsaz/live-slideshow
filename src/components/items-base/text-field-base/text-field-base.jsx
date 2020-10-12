c46ec761d52c57e21397d89407ab831e5d6c4f42
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2020-10-12 19:26:51 +0330

    remove extra props from TextField

--- src/components/items-base/text-field-base/text-field-base.jsx
+++ src/components/items-base/text-field-base/text-field-base.jsx
@@ -3,36 +3,10 @@ import PropTypes from "prop-types";
 // Material-UI
 import TextField from "@material-ui/core/TextField";
 
-export default function TextFieldBase({
-  type,
-  name,
-  value,
-  onChange,
-  label,
-  variant,
-  color,
-  ...rest
-}) {
-  return (
-    <TextField
-      type={type}
-      name={name}
-      value={value}
-      onChange={onChange}
-      label={label}
-      variant={variant}
-      color={color}
-      {...rest}
-    />
-  );
+export default function TextFieldBase({ ...rest }) {
+  return <TextField {...rest} />;
 }
 
 TextField.propTypes = {
-  type: PropTypes.string,
-  name: PropTypes.string,
-  value: PropTypes.any,
-  onChange: PropTypes.func,
-  label: PropTypes.string,
-  variant: PropTypes.string,
-  color: PropTypes.string,
+  rest: PropTypes.any,
 };
