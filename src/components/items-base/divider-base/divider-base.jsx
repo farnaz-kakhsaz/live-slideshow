b3f8d7b945e0ec752f7669fd78545dfcb2eb33ca
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2020-10-14 22:12:21 +0330

    remove extra prop

--- src/components/items-base/divider-base/divider-base.jsx
+++ src/components/items-base/divider-base/divider-base.jsx
@@ -3,11 +3,10 @@ import PropTypes from "prop-types";
 // Material-UI
 import Divider from "@material-ui/core/Divider";
 
-export default function DividerBase({ children, ...rest }) {
-  return <Divider {...rest}>{children}</Divider>;
+export default function DividerBase({ ...rest }) {
+  return <Divider {...rest} />;
 }
 
 DividerBase.propTypes = {
-  children: PropTypes.node,
   rest: PropTypes.any,
 };
