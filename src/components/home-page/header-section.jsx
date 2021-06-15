b0fe6aed9b0352a4d117362e308646c9427ef307
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2021-06-15 15:38:41 +0430

    add text to the header section

--- src/components/home-page/header-section.jsx
+++ src/components/home-page/header-section.jsx
@@ -4,18 +4,29 @@ import BoxBase from "../items-base/box-base/box-base";
 
 export default function HeaderSection() {
   return (
-    <BoxBase
-      display="inline-block"
-      fontSize={{ xs: 32, sm: 42, md: 52 }}
-      fontWeight="fontWeightBold"
-      component="h1"
-      borderBottom="3px solid"
-      my="0"
-    >
-      Go ahead and add or remove photos
-      <span role="img" aria-label="winking face">
-        &#128521;
-      </span>
-    </BoxBase>
+    <>
+      <BoxBase
+        fontSize={{ xs: 32, sm: 42, md: 52 }}
+        fontWeight="fontWeightBold"
+        component="h1"
+        my="0"
+      >
+        This is my React slideshow library
+      </BoxBase>
+      <BoxBase
+        display="inline-block"
+        fontSize={{ xs: 24, sm: 32, md: 42 }}
+        fontWeight="fontWeightBold"
+        component="h2"
+        borderBottom="3px solid"
+        mt="10px"
+        mb="0"
+      >
+        Go ahead and add or remove photos
+        <span role="img" aria-label="winking face">
+          &#128521;
+        </span>
+      </BoxBase>
+    </>
   );
 }
